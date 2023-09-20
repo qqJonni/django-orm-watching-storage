15394c1cb7185353bb061a72a36d6bdaf0c22f28
Valeriy Lvov <tepliy_shov@mail.ru>
2023-09-21 00:06:04 +0500

    get_object_or_404()

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,10 +1,11 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.filter(passcode=passcode)
+    passcard = get_object_or_404(Passcard, passcode=passcode)
     visits_by_this_passcard = Visit.objects.filter(passcard=passcard)
     this_passcard_visits = list()
     for visit in visits_by_this_passcard:
